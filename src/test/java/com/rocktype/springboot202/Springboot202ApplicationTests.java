f56dfc81c39a215d9c3fe43604bdd6849a7690e4
GBTC440013UR <GBTC440013UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-03-09 18:51:55 -0400

    Initial commit

---
+++ src/test/java/com/rocktype/springboot202/Springboot202ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rocktype.springboot202;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot202ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
